7aabcb5d8213d1d65140aa44a59fc0d20d185dc6
Toru <toru.inoue@kissaki.tv>
2016-01-31 01:25:17 +0900

    add resources.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication {
+    public class Program {
+        public static void Main(string[] args) {
+			System.Threading.Thread d;	
+			Task s = Task.Run(() => {
+				while (true) {
+					Console.WriteLine("Hello World!");
+				}
+			});
+
+			s.Wait();
+			
+			Console.WriteLine("Hello World2!");
+        }
+    }
+}
